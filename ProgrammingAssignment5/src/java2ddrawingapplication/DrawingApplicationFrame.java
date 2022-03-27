1284f374f05fa526f69ad0dc6adc71048cf47835
wcn4 <bill.neubauer.4@gmail.com>
2022-03-27 02:05:34 -0400

    Added most of the event handlers for the buttons. Have yet to add the final drawing functionality.

--- ProgrammingAssignment5/src/java2ddrawingapplication/DrawingApplicationFrame.java
+++ ProgrammingAssignment5/src/java2ddrawingapplication/DrawingApplicationFrame.java
@@ -5,31 +5,12 @@
  */
 package java2ddrawingapplication;
 
-import java.awt.BasicStroke;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GradientPaint;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridLayout;
-import java.awt.Paint;
-import java.awt.Point;
-import java.awt.Stroke;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JColorChooser;
-import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
 
 /**
  *
@@ -45,22 +26,44 @@ public class DrawingApplicationFrame extends JFrame
     private final JPanel topPanel;
 
     private final JPanel drawPanel;
-    private final JPanel statusLabel;
+    
     
     // create the widgets for the firstLine Panel.
     private final JPanel shapesPanel;
-    private final JLabel firstLineText;
-    private final JComboBox shapeSelection;
-    private final JButton firstColor;
-    private final JButton secondColor;
-    private final JButton undo;
-    private final JButton clear;
+    private final JComboBox shapeSelectionBox;
+    private final JButton firstColorButton;
+    private final JButton secondColorButton;
+    private final JButton undoButton;
+    private final JButton clearButton;
     //create the widgets for the secondLine Panel.
     private final JPanel optionsPanel;
+    private final JCheckBox filledBox;
+    private final JCheckBox gradientBox;
+    private final JCheckBox dashedBox;
+    private final JSpinner lineWidthScroll;
+    private final JSpinner dashLengthScroll;
+    
+    
     // Variables for drawPanel.
-
+    private int xPos;
+    private int yPos;
+    private enum Shape{
+        Line,
+        Rectangle,
+        Oval
+    }
+    private Shape shape;
+    private Color firstColor;
+    private Color secondColor;
+    private boolean filled = false;
+    private boolean gradient = false;
+    private boolean dashed = false;
+    private int lineWidth = 1;
+    private int dashLength = 1;
+    private ArrayList<Shape> shapesDrawn;
+    
     // add status label
-  
+    private JLabel statusLabel;
     // Constructor for DrawingApplicationFrame
     public DrawingApplicationFrame()
     {
@@ -71,35 +74,136 @@ public class DrawingApplicationFrame extends JFrame
         
         topPanel = new JPanel();
         topPanel.setLayout(new GridLayout(2, 1));
+        //topPanel.setBackground(new Color(50, 200, 200));
+        //topPanel.setBackground(Color.CYAN);
         
         // add widgets to panels
         shapesPanel = new JPanel();
+        shapesPanel.setBackground(Color.CYAN);
         shapesPanel.setLayout(new FlowLayout());
-        firstLineText = new JLabel("Shape:");
         String[] shapes = {"Line", "Rectangle", "Oval"};
-        shapeSelection = new JComboBox(shapes);
-        firstColor = new JButton("1st Color...");
-        secondColor = new JButton("2nd Color...");
-        undo = new JButton("Undo");
-        clear = new JButton("Clear");
+        shapeSelectionBox = new JComboBox(shapes);
+        shapeSelectionBox.addItemListener(
+                new ItemListener(){
+                    @Override
+                    public void itemStateChanged(ItemEvent event){
+                        if(event.getStateChange() == ItemEvent.SELECTED){
+                            switch(shapeSelectionBox.getSelectedIndex()){
+                                case 0:
+                                    shape = Shape.Line;
+                                    break;
+                                case 1:
+                                    shape = Shape.Rectangle;
+                                    break;
+                                case 2:
+                                    shape = Shape.Oval;
+                                    break;
+                                default:
+                                   shape = Shape.Line;
+                                   break;
+                            }
+                        }
+                    }
+                }
+        );
         
-        shapesPanel.add(firstLineText);
-        shapesPanel.add(shapeSelection);
-        shapesPanel.add(firstColor);
-        shapesPanel.add(secondColor);
-        shapesPanel.add(undo);
-        shapesPanel.add(clear);
+        firstColorButton = new JButton("1st Color...");
+        firstColorButton.addActionListener(
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent event){
+                        firstColor = JColorChooser.showDialog(DrawingApplicationFrame.this, 
+                                "Choose a Color", firstColor);
+                    }
+                }
+        );
+        secondColorButton = new JButton("2nd Color...");
+        secondColorButton.addActionListener(
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent event){
+                        secondColor = JColorChooser.showDialog(DrawingApplicationFrame.this, 
+                                "Choose a Color", secondColor);
+                    }
+                }
+        );
+        //Not sure what to do for these just yet.
+        undoButton = new JButton("Undo");
+        clearButton = new JButton("Clear");
+        
+        
+        shapesPanel.add(new JLabel("Shape:"));
+        shapesPanel.add(shapeSelectionBox);
+        shapesPanel.add(firstColorButton);
+        shapesPanel.add(secondColorButton);
+        shapesPanel.add(undoButton);
+        shapesPanel.add(clearButton);
         
         topPanel.add(shapesPanel);
         
         optionsPanel = new JPanel();
+        optionsPanel.setBackground(Color.CYAN);
+        optionsPanel.setLayout(new FlowLayout());
+        filledBox = new JCheckBox("Filled");
+        filledBox.addActionListener(
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent event){
+                        filled = !filled;
+                    }
+                }
+        );
+        gradientBox = new JCheckBox("Use Gradient");
+        gradientBox.addActionListener(
+                new ActionListener(){
+                    @Override
+                    public void actionPerformed(ActionEvent event){
+                        gradient = !gradient;
+                    }
+                }
+        );
+        dashedBox = new JCheckBox("Dashed");
+        lineWidthScroll = new JSpinner();
+        //Sets it such that initial value is 1, minimum is 1, max is 100, step is 1
+        lineWidthScroll.setModel(new SpinnerNumberModel(1, 1, 100, 1));
+        lineWidthScroll.addChangeListener(
+                //Since JSpinners do not use ActionListeners, use ChangeListeners
+                //Instead
+                new ChangeListener(){
+                    @Override
+                    public void stateChanged(ChangeEvent event){
+                        lineWidth = (int)lineWidthScroll.getValue();
+                    }
+                }
+        );
+        dashLengthScroll = new JSpinner();
+        //Sets it such that initial value is 1, minimum is 1, max is 100, step is 1
+        dashLengthScroll.setModel(new SpinnerNumberModel(1, 1, 100, 1));
+        dashLengthScroll.addChangeListener(
+                new ChangeListener(){
+                    @Override
+                    public void stateChanged(ChangeEvent event){
+                        dashLength = (int)dashLengthScroll.getValue();
+                    }
+                }
+        );
         
+        optionsPanel.add(new JLabel("Options:"));
+        optionsPanel.add(filledBox);
+        optionsPanel.add(gradientBox);
+        optionsPanel.add(dashedBox);
+        optionsPanel.add(new JLabel("Line Width:"));
+        optionsPanel.add(lineWidthScroll);
+        optionsPanel.add(new JLabel("Dash Length:"));
+        optionsPanel.add(dashLengthScroll);
         topPanel.add(optionsPanel);
         
         
-        drawPanel = new JPanel();
+        drawPanel = new DrawPanel();
+        drawPanel.setBackground(Color.WHITE);
+
         
-        statusLabel = new JPanel();
+        statusLabel = new JLabel("(0, 0)");
         
         
         add(topPanel, BorderLayout.NORTH);
@@ -121,7 +225,7 @@ public class DrawingApplicationFrame extends JFrame
             //Use a border layout for this
         //add listeners and event handlers
     }
-
+    
     // Create event handlers, if needed
 
     // Create a private inner class for the DrawPanel.
@@ -130,6 +234,9 @@ public class DrawingApplicationFrame extends JFrame
 
         public DrawPanel()
         {
+            MouseHandler handler = new MouseHandler();
+            addMouseListener(handler);
+            addMouseMotionListener(handler);
         }
 
         public void paintComponent(Graphics g)
@@ -147,6 +254,7 @@ public class DrawingApplicationFrame extends JFrame
 
             public void mousePressed(MouseEvent event)
             {
+                
             }
 
             public void mouseReleased(MouseEvent event)
@@ -156,13 +264,19 @@ public class DrawingApplicationFrame extends JFrame
             @Override
             public void mouseDragged(MouseEvent event)
             {
+                
             }
 
             @Override
             public void mouseMoved(MouseEvent event)
             {
+                xPos = event.getX();
+                yPos = event.getY();
+                statusLabel.setText("(" + xPos + "," + yPos + ") " + lineWidth);
             }
         }
 
     }
+    
+    
 }
